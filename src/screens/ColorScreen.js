439124703108f398485aa823d0fddb483f73acdd
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-06-17 23:23:20 -0400

    square screen color

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -4,8 +4,7 @@ import { View, StyleSheet, Button, FlatList } from 'react-native';
 const ColorScreen = () => {
 
 	const [colors, setColors] = useState([]);
-	
-	console.log(colors); 
+
 
 	return (
 		<View>

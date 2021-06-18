dee11019f3870a6e2abddae9e41a1bde039b83ad
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-06-17 21:55:26 -0400

    added random color generator

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
 const CounterScreen = () => {
+
+	const [counter, setCounter] = useState(0); 
+	
 	return (
-		<View>
-			<Text>Current Count: </Text>
+		<View style={{
+			flex: 1,
+			justifyContent: 'center',
+			alignItems: 'center',
+			margin: 5
+		}}>
+			<Button
+				title="Increase"
+				onPress={() => {
+					setCounter(counter + 1);
+				}}
+			/>
+			<Button
+				title="Decrease"
+				onPress={() => {
+					setCounter(counter - 1);
+				}}
+			/>
+			<Text style={{
+				flex: 1,
+				justifyContent: 'center',
+				alignItems: 'center',
+				fontWeight: 'bold',
+				marginTop: 15
+			}}>Current Count: {counter} </Text>
 		</View>
 	);
 };
